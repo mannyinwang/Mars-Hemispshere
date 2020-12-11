466dacf5187e4c73a32afa332794ddb435386f1b
mannyinwang <33712694+mannyinwang@users.noreply.github.com>
2020-12-11 00:31:15 -0800

    made changes to this file

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+
+# Dependencies
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping
 
 app = Flask(__name__)
 
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    mars = mongo.db.mars.find_one()
+    print(mars)
+    return render_template('index.html', mars=mars)
+
+
+@app.route('/scrape')
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    print(mars_data)
+    mars.update({}, mars_data, upsert=True)
+    return "Scraping Successful"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
